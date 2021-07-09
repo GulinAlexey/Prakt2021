f012d3958a0a3d6dd3fe0f0acbcace0ae85665fd
GulinAlexey <gulin-2001@mail.ru>
2021-07-10 02:55:03 +0700

    Реализован вывод картинки в label

--- Forest/mainwidget.cpp
+++ Forest/mainwidget.cpp
@@ -6,6 +6,8 @@ MainWidget::MainWidget(QWidget *parent)
     , ui(new Ui::MainWidget)
 {
     ui->setupUi(this);
+    QPixmap sun = QPixmap("sun.png"); //объект с картинкой
+    ui->pogoda->setPixmap(sun); // вывести картинку в лейбл
 }
 
 MainWidget::~MainWidget()
