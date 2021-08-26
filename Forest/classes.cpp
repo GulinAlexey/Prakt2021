46404ea5b9eb31ba2d76c30379d0b7c14be2e748
GulinAlexey <gulin-2001@mail.ru>
2021-08-26 20:21:25 +0700

    Добавлены массивы координат точек перемещения лесника и браконьеров, когда они не видят цель (заготовка инициализации

--- Forest/classes.cpp
+++ Forest/classes.cpp
@@ -929,3 +929,57 @@ int Invaders::get_corner(int num, int coord) //получить значение
 {
     return corners[num][coord];
 }
+
+void Forester::set_route_points() //установить координаты для маршрута патрулирования лесника
+{
+    route_points[0][X_CORNER]=;
+    route_points[0][Y_CORNER]=;
+
+    route_points[1][X_CORNER]=;
+    route_points[1][Y_CORNER]=;
+
+    route_points[2][X_CORNER]=;
+    route_points[2][Y_CORNER]=;
+
+    route_points[3][X_CORNER]=;
+    route_points[3][Y_CORNER]=;
+
+    route_points[4][X_CORNER]=;
+    route_points[4][Y_CORNER]=;
+
+    route_points[5][X_CORNER]=;
+    route_points[5][Y_CORNER]=;
+
+    route_points[6][X_CORNER]=;
+    route_points[6][Y_CORNER]=;
+
+    route_points[7][X_CORNER]=;
+    route_points[7][Y_CORNER]=;
+}
+
+void Invaders::set_route_points() //установить координаты для маршрута перемещения браконьеров
+{
+    route_points[0][X_CORNER]=;
+    route_points[0][Y_CORNER]=;
+
+    route_points[1][X_CORNER]=;
+    route_points[1][Y_CORNER]=;
+
+    route_points[2][X_CORNER]=;
+    route_points[2][Y_CORNER]=;
+
+    route_points[3][X_CORNER]=;
+    route_points[3][Y_CORNER]=;
+
+    route_points[4][X_CORNER]=;
+    route_points[4][Y_CORNER]=;
+
+    route_points[5][X_CORNER]=;
+    route_points[5][Y_CORNER]=;
+
+    route_points[6][X_CORNER]=;
+    route_points[6][Y_CORNER]=;
+
+    route_points[7][X_CORNER]=;
+    route_points[7][Y_CORNER]=;
+}
